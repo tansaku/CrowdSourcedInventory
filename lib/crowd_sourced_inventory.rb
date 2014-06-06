941ffe6b9d0a0267b47cd94c24113e53896b9515
Sam Joseph <tansaku@gmail.com>
2014-06-06 13:36:01 +0100

    adds contents method

--- lib/crowd_sourced_inventory.rb
+++ lib/crowd_sourced_inventory.rb
@@ -1,7 +1,13 @@
 class CrowdSourcedInventory
 
+  attr_reader :contents
+
+  def initialize
+    @contents = []
+  end
+
   def add_item(item)
-     ['apple']
+    @contents << item 
   end
 
 end
--- spec/crowd_sourced_inventory_spec.rb
+++ spec/crowd_sourced_inventory_spec.rb
@@ -2,9 +2,22 @@ require 'crowd_sourced_inventory'
 
 describe CrowdSourcedInventory do
   let(:csi) {CrowdSourcedInventory.new}
-  it "can accept a new inventory item" do
-    expect(csi.add_item('apple')).to eq ['apple'] 
-  end 
+
+  it "has no contents by default" do
+    expect(csi.contents).to eq []
+  end
+
+  it "can accept more than one item" do
+    csi.add_item('apple')
+    csi.add_item('pear')
+    expect(csi.contents).to eq ['apple','pear']
+  end
+
+  it "can show the contents" do
+    csi.add_item('apple')
+    expect(csi.contents).to eq ['apple']
+  end
+
 
 
 
